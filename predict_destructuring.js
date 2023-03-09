b948e9a4a9e2ec28b4170f8362b7b1de15a68bc4
Brittney Perez <brittney.mp2005@gmail.com>
2023-03-08 23:22:03 -0500

    upload asgmt 1

---
+++ predict_destructuring.js
@@ -0,0 +1,53 @@
+// ? PROBLEM 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)			// >>> Tesla
+console.log(otherRandomCar)     // >>> Mercedes
+
+// ? PROBLEM 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+// console.log(name);      // >>> ReferenceError: name is not defined
+console.log(otherName);    // >>> Elon
+
+// ? PROBLEM 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;
+//Predict the output
+console.log(password);          // >>> 12345
+console.log(hashedPassword);    // >>> undefined
+
+// ? PROBLEM 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);   // >>> false
+console.log(first == third);    // >>> true
+
+// ? PROBLEM 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;               // lastTest >> key >> 'value'
+const { secondKey } = lastTest;         // lastTest >> secondKey >> [ 1, 5, 1, 8, 3, 3 ]
+const [ ,willThisWork] = secondKey;     // lastTest >> secondKey >> 5
+//Predict the output
+console.log(key);           // string: >>> value
+console.log(secondKey);     // array:  >>> [ 1, 5, 1, 8, 3, 3 ]
+console.log(secondKey[0]);  // index:  >>> 1
+console.log(willThisWork);  // index:  >>> 5
